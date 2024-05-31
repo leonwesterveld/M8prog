b12fb6de302388e5a577a2d3f932cfc2e477d498
Leon Westerveld <114667043+leonwesterveld@users.noreply.github.com>
2024-05-31 10:59:49 +0200

    les 4 niet af

---
+++ themes/newnew/index.php
@@ -0,0 +1,7 @@
+get_header();
+get_footer();
+<?php
+echo '<pre style="background:#0f0; padding: 2rem; width:100%;">';
+print_r( 'dit is mijn index pagina' );
+echo '</pre>';
+die(__FILE__.':'.__LINE__);
