b07a42497ba6dad488155e46f503571377319b3b
Leon Westerveld <114667043+leonwesterveld@users.noreply.github.com>
2024-05-31 14:52:30 +0200

    child is opgegroeid

--- themes/newnew/index.php
+++ themes/newnew/index.php
@@ -10,6 +10,28 @@
         </p>
     </div>
 </main>
+<?php 
+// Start with a if statement
+if ( have_posts() ) :
+
+	// Loop trough the posts
+	while ( have_posts() ) :
+	    // Load the data post data
+		the_post();
+		?>
+        <main class="container my-5">
+            <h1 class="mt-5"><?php the_title() ?></h1>
+            <div class="col-lg-8 px-0">
+                <?php the_excerpt() ?>
+            </div>
+        </main>
+	    <?php
+	endwhile;
+
+else: 
+    echo 'Volgens mij zoek je iets dat er niet is';
+endif;
+?>
 <?php get_footer();?>
 
 
